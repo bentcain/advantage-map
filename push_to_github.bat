1f3c2379e8524bfd6c7c23322bb8a71cce0f1afd
Ben Cain <ben@bluejackresources.com>
2025-05-17 17:32:32 -0500

    Update site content

--- push_to_github.bat
+++ push_to_github.bat
@@ -13,7 +13,7 @@ git push
 for %%I in ("%cd%") do set "FOLDERNAME=%%~nxI"
 
 :: Open the live GitHub Pages site
-start https://bentcain.github.io/!FOLDERNAME!/
+start https://bentcain.github.io/advantage-map/
 
 echo ✅ Done.
 pause
