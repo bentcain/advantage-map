33484fc881973657d1bdfdf4183ef291b65fb406
Ben Cain <ben@bluejackresources.com>
2025-05-22 09:01:53 -0500

    Update site content

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyD7JkobzpJcrOHU_fNwXuEQqdYL1iAiiRQ",
   authDomain: "parcel-notes-system.firebaseapp.com",
-  databaseURL: "https://parcel-notes-system-default-rtdb.firebaseio.com",
   projectId: "parcel-notes-system",
   storageBucket: "parcel-notes-system.appspot.com",
   messagingSenderId: "538142262863",
   appId: "1:538142262863:web:89dfd4cf60bb2dc4694c3a",
-  measurementId: "G-GQ3CQ9EKPH"
+  measurementId: "G-GQ3CQ9EKPH",
+  databaseURL: "https://parcel-notes-system-default-rtdb.firebaseio.com"
 };
